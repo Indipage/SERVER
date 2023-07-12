ceb017baf58c1280b892b670d3088f61014b8691
sunseo18 <sunseo1818@gmail.com>
2023-07-13 00:24:58 +0900

    [ADD] embeddedId 생성 팩토리 메서드 구현 #36

--- src/main/java/indipage/org/indipage/domain/Relation/ArticleBookmarkRelationId.java
+++ src/main/java/indipage/org/indipage/domain/Relation/ArticleBookmarkRelationId.java
@@ -1,13 +1,18 @@
 package indipage.org.indipage.domain.Relation;
 
+import indipage.org.indipage.domain.Article;
+import indipage.org.indipage.domain.Space;
+import indipage.org.indipage.domain.User;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class ArticleBookmarkRelationId implements Serializable {
 
@@ -16,4 +21,8 @@ public class ArticleBookmarkRelationId implements Serializable {
 
     @Column(name = "user_id", nullable = false)
     private Long userId;
+
+    public static ArticleBookmarkRelationId newInstance(Article article, User user) {
+        return new ArticleBookmarkRelationId(article.getId(), user.getId());
+    }
 }
