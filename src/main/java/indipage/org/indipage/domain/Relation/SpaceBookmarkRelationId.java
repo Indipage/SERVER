71777d5e857570c23765c02de3e71bc8f73327d8
sunseo18 <sunseo1818@gmail.com>
2023-07-13 01:27:49 +0900

    [ADD] 공간 북마크 저장을 위한 메서드 생성 #39

--- src/main/java/indipage/org/indipage/domain/Relation/SpaceBookmarkRelationId.java
+++ src/main/java/indipage/org/indipage/domain/Relation/SpaceBookmarkRelationId.java
@@ -1,12 +1,16 @@
 package indipage.org.indipage.domain.Relation;
 
+import indipage.org.indipage.domain.Space;
+import indipage.org.indipage.domain.User;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 public class SpaceBookmarkRelationId implements Serializable {
@@ -16,4 +20,8 @@ public class SpaceBookmarkRelationId implements Serializable {
 
     @Column(name = "space_id", nullable = false)
     private Long spaceId;
+
+    public static SpaceBookmarkRelationId newInstance(User user, Space space) {
+        return new SpaceBookmarkRelationId(user.getId(), space.getId());
+    }
 }
