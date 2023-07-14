6454cb9a4ae0651b2028ee9b66efb012a146c466
Jin409 <sualng123@gmail.com>
2023-07-15 02:27:18 +0900

    [ADD] 유저가 슬라이드한 날짜를 저장하는 칼럼 추가 #55

--- src/main/java/indipage/org/indipage/domain/User.java
+++ src/main/java/indipage/org/indipage/domain/User.java
@@ -2,18 +2,14 @@ package indipage.org.indipage.domain;
 
 import indipage.org.indipage.domain.Relation.ArticleBookmarkRelation;
 import indipage.org.indipage.domain.Relation.SpaceBookmarkRelation;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,6 +24,9 @@ public class User extends CreatedTimeBaseEntity {
 
     @Column
     private String email;
+
+    @Column
+    private LocalDateTime slideAt;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     private List<ArticleBookmarkRelation> articleBookmarkRelations = new ArrayList<>();
