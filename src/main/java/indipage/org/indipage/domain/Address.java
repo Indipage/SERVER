952bad9c2f84a7dcf4726c3cb2959317fdfbbd96
Jin409 <sualng123@gmail.com>
2023-07-18 16:21:51 +0900

    [ADD] 주소에서 건물 번호 관련 컬럼 삭제 및 문자열로 반환하는 메서드 추가 #79

--- src/main/java/indipage/org/indipage/domain/Address.java
+++ src/main/java/indipage/org/indipage/domain/Address.java
@@ -8,12 +8,35 @@ import javax.persistence.Embeddable;
 @Getter
 public class Address {
 
-    private String MetroGovernment;
-    private String BaseGovernment;
-    private String City;
-    private String Town;
-    private String RoadName;
-    private String BuildingNumber;
+    private String metroGovernment;
+    private String baseGovernment;
+    private String city;
+    private String town;
+    private String roadName;
     private String detail;
 
+    public String toString() {
+        StringBuilder fullAddressBuilder = new StringBuilder();
+
+        if (metroGovernment != null) {
+            fullAddressBuilder.append(metroGovernment).append(" ");
+        }
+        if (baseGovernment != null) {
+            fullAddressBuilder.append(baseGovernment).append(" ");
+        }
+        if (city != null) {
+            fullAddressBuilder.append(city).append(" ");
+        }
+        if (town != null) {
+            fullAddressBuilder.append(town).append(" ");
+        }
+        if (roadName != null) {
+            fullAddressBuilder.append(roadName).append(" ");
+        }
+        if (detail != null) {
+            fullAddressBuilder.append(detail);
+        }
+
+        return fullAddressBuilder.toString().trim();
+    }
 }
