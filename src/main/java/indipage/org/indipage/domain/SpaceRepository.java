5d57a54d303330b43d7163d2ec9d0c5246bf0720
Jin409 <sualng123@gmail.com>
2023-07-18 16:20:53 +0900

    [FEAT] SQL FULL MATCH 를 이용해서 공간을 검색하는 기능 구현 #79

--- src/main/java/indipage/org/indipage/domain/SpaceRepository.java
+++ src/main/java/indipage/org/indipage/domain/SpaceRepository.java
@@ -1,8 +1,15 @@
 package indipage.org.indipage.domain;
 
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SpaceRepository extends Repository<Space, Long> {
     Optional<Space> findById(Long id);
+
+    @Query(value = "SELECT * FROM space s WHERE MATCH(s.base_government, s.city, s.metro_government, s.road_name, s.town, s.detail) AGAINST(:searchTerm)", nativeQuery = true)
+    List<Space> searchByAddress(@Param("searchTerm") String searchTerm);
 }
