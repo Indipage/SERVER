189be8f2686feeec8b7826db3fa1e15c4d32e605
Jin409 <sualng123@gmail.com>
2023-07-21 03:52:07 +0900

    [DEL] 사용하지 않는 api 삭제 #90

--- src/main/java/indipage/org/indipage/api/user/controller/UserController.java
+++ src/main/java/indipage/org/indipage/api/user/controller/UserController.java
@@ -8,16 +8,11 @@ import indipage.org.indipage.api.user.controller.dto.response.UserDto;
 import indipage.org.indipage.api.user.service.UserService;
 import indipage.org.indipage.common.dto.ApiResponse;
 import indipage.org.indipage.exception.Success;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -44,14 +39,6 @@ public class UserController {
         userService.receiveTicket(1L, spaceId);
         return ApiResponse.success(Success.CREATE_RECEIVE_TICKET_SUCCESS);
     }
-
-    @PutMapping("/space/{spaceId}/visit")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse updateVisit(@PathVariable final Long spaceId) {
-        userService.visit(1L, spaceId);
-        return ApiResponse.success(Success.UPDATE_VISIT_SUCCESS);
-    }
-
 
     @PutMapping("/weekly/slide")
     @ResponseStatus(HttpStatus.OK)
