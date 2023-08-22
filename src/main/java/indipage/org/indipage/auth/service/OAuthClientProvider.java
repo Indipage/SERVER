0a6ad901f1c3644abf00c0f6743bd04926e52f10
Jin409 <sualng123@gmail.com>
2023-08-22 16:04:50 +0900

    [FEAT] 플랫폼에 따른 client 를 반환하는 기능 구현 #104

---
+++ src/main/java/indipage/org/indipage/auth/service/OAuthClientProvider.java
@@ -0,0 +1,27 @@
+package indipage.org.indipage.auth.service;
+
+import indipage.org.indipage.auth.Platform;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class OAuthClientProvider {
+
+    private static final Map<Platform, OAuthClient> oAuthClientMap = new HashMap<>();
+
+    private final AppleOauthClient appleOauthClient;
+
+    @PostConstruct
+    void initializeOAuthClientMap() {
+        oAuthClientMap.put(Platform.APPLE, appleOauthClient);
+    }
+
+    public OAuthClient getClient(Platform oAuthProvider) {
+        return oAuthClientMap.get(oAuthProvider);
+    }
+}
