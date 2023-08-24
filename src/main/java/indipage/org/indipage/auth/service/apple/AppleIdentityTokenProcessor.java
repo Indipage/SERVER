2da28297c30e851fe3b79794099b3787ab56fc03
Jin409 <sualng123@gmail.com>
2023-08-25 01:08:46 +0900

    [FEAT] 토큰의 헤더값 파싱하는 기능 구현 #108

---
+++ src/main/java/indipage/org/indipage/auth/service/apple/AppleIdentityTokenProcessor.java
@@ -0,0 +1,43 @@
+package indipage.org.indipage.auth.service.apple;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import indipage.org.indipage.auth.utils.EncryptUtils;
+import indipage.org.indipage.exception.Error;
+import indipage.org.indipage.exception.model.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AppleIdentityTokenProcessor {
+
+    @Value("${oauth.apple.iss}")
+    private String iss;
+    @Value("${oauth.apple.client-id}")
+    private String clientId;
+    @Value("${oauth.apple.nonce}")
+    private String nonce;
+    private static final String DELIMITER = "\\.";
+    private static final int HEADER_INDEX = 0;
+    private static final String NONCE_KEY = "nonce";
+
+    private final EncryptUtils encryptUtils;
+
+    public Map<String, String> getParsedHeader(final String identityToken) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String encodedHeader = identityToken.split(DELIMITER)[HEADER_INDEX];
+            String decodedHeader = new String(Base64Utils.decodeFromUrlSafeString(encodedHeader));
+            return mapper.readValue(decodedHeader, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new UnauthorizedException(Error.INVALID_TOKEN_EXCEPTION, Error.INVALID_TOKEN_EXCEPTION.getMessage());
+        }
+    }
+
+
+}
