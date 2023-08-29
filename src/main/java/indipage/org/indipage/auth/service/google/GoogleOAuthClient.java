b98df72da2f57b2ef80602bf494edb3abf368ded
sunseo18 <sunseo1818@gmail.com>
2023-08-30 01:11:32 +0900

    [FEAT] 구글 소셜 로그인 구현 #113

--- src/main/java/indipage/org/indipage/auth/service/google/GoogleOAuthClient.java
+++ src/main/java/indipage/org/indipage/auth/service/google/GoogleOAuthClient.java
@@ -1,2 +1,42 @@
-package indipage.org.indipage.auth.service.google;public class GoogleOAuthClient {
+package indipage.org.indipage.auth.service.google;
+
+import indipage.org.indipage.auth.dto.OAuthUserResponseDto;
+import indipage.org.indipage.auth.service.OAuthClient;
+import indipage.org.indipage.auth.service.PublicKeyGenerator;
+import indipage.org.indipage.auth.service.JWKs;
+import io.jsonwebtoken.Claims;
+import java.security.PublicKey;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+@RequiredArgsConstructor
+public class GoogleOAuthClient implements OAuthClient {
+
+
+    private final GoogleIdTokenProcessor tokenProcessor;
+    private final GooglePublicKeyClient publicKeyClient;
+    private final PublicKeyGenerator publicKeyGenerator;
+
+
+    @Override
+    public OAuthUserResponseDto getUser(final String idToken) {
+        Map<String, String> header = tokenProcessor.getParsedHeader(idToken);
+        JWKs googlePublicKeys = publicKeyClient.getJWKs();
+
+        PublicKey publicKey = publicKeyGenerator.generatePublicKey(header, googlePublicKeys);
+
+        Claims claims = tokenProcessor.extractClaims(idToken, publicKey);
+
+        return OAuthUserResponseDto.generateAppleUserResponseDto(claims.get("email", String.class));
+    }
+
+    @FeignClient(name = "google-public-key-client", url = "https://www.googleapis.com/oauth2/v3")
+    public interface GooglePublicKeyClient {
+        @GetMapping("/certs")
+        JWKs getJWKs();
+    }
 }
