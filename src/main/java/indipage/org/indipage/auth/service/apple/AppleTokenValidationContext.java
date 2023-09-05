5a1ec2090cb0743d2372b7ea4ddad29bac52b468
Jin409 <sualng123@gmail.com>
2023-09-05 20:29:08 +0900

    [ADD] AppleTokenValidationContext 추가 #116

---
+++ src/main/java/indipage/org/indipage/auth/service/apple/AppleTokenValidationContext.java
@@ -0,0 +1,15 @@
+package indipage.org.indipage.auth.service.apple;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "oauth.apple")
+public class AppleTokenValidationContext {
+
+    private final String iss;
+    private final String clientId;
+    private final String nonce;
+}
