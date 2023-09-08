efac9044df8d9057b370d9f12c8fe2cf0ad992a8
Jin409 <sualng123@gmail.com>
2023-09-08 16:48:44 +0900

    [FIX] 생성자 바인딩 오류 해결 #116

--- src/main/java/indipage/org/indipage/auth/service/apple/AppleTokenValidationContext.java
+++ src/main/java/indipage/org/indipage/auth/service/apple/AppleTokenValidationContext.java
@@ -3,8 +3,10 @@ package indipage.org.indipage.auth.service.apple;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
+@ConstructorBinding
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "oauth.apple")
 public class AppleTokenValidationContext {
