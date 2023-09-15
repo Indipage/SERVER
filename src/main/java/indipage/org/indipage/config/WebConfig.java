0c4d0b689251defc5443245fdbb3ad6ded914af6
sunseo18 <sunseo1818@gmail.com>
2023-09-15 13:55:44 +0900

    [ADD] argument resolver 추가 설정 #115

--- src/main/java/indipage/org/indipage/config/WebConfig.java
+++ src/main/java/indipage/org/indipage/config/WebConfig.java
@@ -1,2 +1,20 @@
-package indipage.org.indipage.config;public class WebConfig {
+package indipage.org.indipage.config;
+
+import indipage.org.indipage.config.resolver.UserIdResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserIdResolver userIdResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdResolver);
+    }
 }
